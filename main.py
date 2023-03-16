2afa93908acc24c23746137146075962be176593
EvanG13 <emgiampaoli@gmail.com>
2023-03-16 15:39:02 -0700

    update main to parse file creating list of valid commands

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 
+
 def process_data():
-    print("processing data")
+    # set of all valid commands
+    # inside sets for future expansion to valid actions and records
+    valid_action = {"ADD", "DELETE"}
+    valid_record = {"PATIENT", "EXAM"}
+
+    # list of commands
+    commands_list = []
+
+    # open file then read line by line
+    file = open("data1")
+    for command in file:
+        command_seg = command.split()
+        # check if the action and record are valid
+        # if not valid, then start processing the next line of the file
+        if command_seg[0] not in valid_action or command_seg[1] not in valid_record:
+            continue
+        # action and record are valid
+        commands_list.append(command)
+
+    # print list of valid commands
+    print(commands_list)
+
+    # cleanup
+    file.close()
 
 
 def main():
