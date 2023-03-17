ccb1faea380dfc531b9c19ef86c608beebf560f2
EvanG13 <emgiampaoli@gmail.com>
2023-03-17 15:29:35 -0700

    initial implementation of program complete

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # data-aggregation
+
+## Project Description
+We want you to create a simple service that aggregates patient and exam data. Your project
+should be able to read in a .txt file that conforms to the specifications below and then outputs a
+summary as described in the “Output Specification” section of this document.
+
+
+### Input File Specification
+The input file will be a .txt file containing a series of instructions that will instruct your program to:
+add a patient record, add an exam record, delete a patient record, or delete an exam record.
+There will be one instruction on each line in the file. Each instruction can further be broken down
+into a series of space-delimited segments (detailed below for each type of instruction):
+
+### Add a Patient Record
+The command to add a new patient record looks like this:
+
+<mark>ADD PATIENT 123 JOHN DOE</mark>
+
+The command will begin with the word ADD, followed by the word PATIENT, followed by a
+patient identifier, and finally any remaining text on this line should be considered the patient’s
+name.
+
+**Edge case**: If a patient with the given identifier already exists, then ignore this command
+and continue processing the rest of the file.
+
+### Add an Exam Record
+The command to add a new exam record looks like this:
+
+<mark>ADD EXAM 123 456</mark>
+
+The command will begin with the word ADD, followed by the word EXAM, followed by a patient
+identifier, and then an exam identifier.
+
+**Edge case**: If a patient with the given patient identifier does not already exist, then ignore
+this command and continue processing the rest of the file.
+
+**Edge case**: If an exam with the given exam identifier already exists, then ignore this
+command and continue processing the rest of the file
+
+### Delete a Patient Record
+The command to delete a patient record looks like this:
+
+<mark>DEL PATIENT 123</mark>
+
+The command will begin with the word DEL, followed by the word PATIENT, followed by a
+patient identifier. This command should remove the patient record from your system along with
+any exams that have been created for the patient.
+
+**Edge case**: If a patient with the given identifier does not already exist, then ignore
+this command and continue processing the rest of the file
+
+### Delete an Exam Record
+
+The command to delete an exam record looks like this:
+
+<mark>DEL EXAM 456</mark>
+
+The command will begin with the word DEL, followed by the word EXAM, followed by an exam
+identifier. This command should remove the exam record from your system.
+
+**Edge case**: If an exam with the given identifier does not already exist, then ignore
+this command and continue processing the rest of the file.
--- main.py
+++ main.py
@@ -1,45 +1,139 @@
 import sys
 
 
+def process_summary(patients):
+    """
+    process a summary of all the patient records: names, id and exam count
+    :param patients: dictionary containing the existing patients
+    :return: a string that is a list of all the patient records, returns empty string if empty patients record
+    example: Name: PATIENT_NAME, Id: PATIENT_ID, Exam Count: EXAM_COUNT
+    """
+
+    return_str = ""
+    for patient_id in patients:
+        patient_name = patients[patient_id][0]
+        exam_count = len(patients[patient_id][1])
+        return_str += "Name: " + patient_name + ", Id: " + str(patient_id) + ", Exam Count: " + str(exam_count) + "\n"
+
+    return return_str
+
+
 def add_patient(patients, patient_id, patient_name):
+    """
+    add a patient record to patients dict
+    used by command : ADD PATIENT
+    :param patients: dict of patients
+    :param patient_id: patient id
+    :param patient_name: patient name
+    """
+
+    # check if patient id is not in the patients dict to ensure that
+    # only want to add new patients
+    # otherwise, do not add and continue processing
     if patient_id not in patients:
-        patients[patient_id] = patient_name
+        patients[patient_id] = [patient_name, set()]
+
+
+def add_exam(patients, patient_id, exam_id):
+    """
+    add a new exam record
+    used by command : ADD EXAM
+    :param patients: dict of patients
+    :param patient_id: patient id
+    :param exam_id: exam id
+    """
+
+    # check if the patient id is in the patients dict
+    # only want to add an exam for patients who exist
+    # if patient exists, add the exam id to their exam_id set
+    # uses a set to hold exam ids in order to prevent duplicates
+    if patient_id in patients:
+        patients[patient_id][1].add(exam_id)
+
+
+def delete_patient(patients, patient_id):
+    """
+    delete patient from the dict of patients
+    used by command : DEL PATIENT
+    :param patients: dict of patients
+    :param patient_id:  patient id
+    """
+
+    # check if the given patient id is in the patients dict
+    # need to check in order to ensure that we only attempt to delete a patient that exists
+    # if they do exist, delete the patient
+    # otherwise, do not delete
+    if patient_id in patients:
+        patients.pop(patient_id)
+
+
+def delete_exam(patients, exam_id):
+    """
+    delete the given exam
+    :param patients: dict of patients
+    :param exam_id: exam id
+    """
+
+    # iterate through the patients dict searching for the patient who owns that exam record
+    # if an owner of that exam record is found, delete the exam record
+    # otherwise, do not delete
+    for patient_id in patients:
+        if exam_id in patients[patient_id][1]:
+            patients[patient_id][1].remove(exam_id)
+            break
 
 
 def process_instruction(patients, instruction):
+    """
+    process the passed in valid instruction
+    :param patients: dict of patients
+    :param instruction: valid instruction
+    """
 
-    # dictionary containing all the current patients
-    # Exam patientID examID
-    #   key = patient
-    #   val = number of exams
-    exams = {}
-
+    # split the valid instruction into a list
     instr_list = instruction.split()
 
+    # the command and record are at these two positions for every valid instruction
     command = instr_list[0]
     record = instr_list[1]
+
+    # start processing instructions
+    # ADD PATIENT patient_id name
     if command == "ADD" and record == "PATIENT":
         patient_id = instr_list[2]
         patient_name = ' '.join(instr_list[3:])
         add_patient(patients, patient_id, patient_name)
+    # ADD EXAM patientID examID
     elif command == "ADD" and record == "EXAM":
-        print("add exam")
+        patient_id = instr_list[2]
+        exam_id = instr_list[3]
+        add_exam(patients, patient_id, exam_id)
+    # DEL PATIENT patient_iD
+    elif command == "DEL" and record == "PATIENT":
+        patient_id = instr_list[2]
+        delete_patient(patients, patient_id)
+    # DEL EXAM exam_id
+    elif command == "DEL" and record == "EXAM":
+        exam_id = instr_list[2]
+        delete_exam(patients, exam_id)
 
 
 def process_file(file_name):
+    """
+    process the given file of Patient and Exam records
+    :param file_name: name of input file
+    """
+
     # set of all valid command actions and records
     valid_command = {"ADD", "DEL"}
     valid_record = {"PATIENT", "EXAM"}
 
-    # list of commands
-    commands_list = []
-
     # dictionary containing all the current patients
     #   key = id
-    #   val = [name, number exams]
+    #   val = [name, set() of exam ids]
     patients = {}
 
-    # parse file
+    # parse file line by line
     try:
         file = open(file_name)
         for line in file:
@@ -49,13 +143,10 @@ def process_file(file_name):
 
             # check if line is a valid instruction
             # if not valid, then start processing the next line of the file
-            if line_seg[0] not in valid_command or line_seg[1] not in valid_record:
-                continue
-
-            # valid instruction
-            # therefore, process the instruction
-            process_instruction(patients, line)
-        print(patients)
+            if line_seg[0] in valid_command and line_seg[1] in valid_record:
+                # valid instruction
+                # therefore, process the instruction
+                process_instruction(patients, line)
     except FileNotFoundError:
         print("File :", file_name + " does not exist")
         sys.exit(-1)
@@ -63,9 +154,14 @@ def process_file(file_name):
     # cleanup
     file.close()
 
+    # input file of instructions was successfully processed
+    # therefore, return the summary as a string
+    return process_summary(patients)
+
 
 def main(argv):
-    process_file(argv)
+    summary = process_file(argv)
+    print(summary)
 
 
 if __name__ == "__main__":
