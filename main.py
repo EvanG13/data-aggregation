72eb5671b969241c35dae1db90ee9b96d1c44503
EvanG13 <emgiampaoli@gmail.com>
2023-03-18 18:05:53 -0700

    update exam and patient deletion

--- main.py
+++ main.py
@@ -34,10 +34,11 @@ def add_patient(patients, patient_id, patient_name):
         patients[patient_id] = [patient_name, set()]
 
 
-def add_exam(patients, patient_id, exam_id):
+def add_exam(patients, exams, patient_id, exam_id):
     """
     add a new exam record
     used by command : ADD EXAM
+    :param exams: dict of exams
     :param patients: dict of patients
     :param patient_id: patient id
     :param exam_id: exam id
@@ -47,14 +48,16 @@ def add_exam(patients, patient_id, exam_id):
     # only want to add an exam for patients who exist
     # if patient exists, add the exam id to their exam_id set
     # uses a set to hold exam ids in order to prevent duplicates
-    if patient_id in patients:
+    if patient_id in patients and exam_id not in exams:
         patients[patient_id][1].add(exam_id)
+        exams[exam_id] = patient_id
 
 
-def delete_patient(patients, patient_id):
+def delete_patient(patients, exams, patient_id):
     """
     delete patient from the dict of patients
     used by command : DEL PATIENT
+    :param exams:
     :param patients: dict of patients
     :param patient_id:  patient id
     """
@@ -64,28 +67,38 @@ def delete_patient(patients, patient_id):
     # if they do exist, delete the patient
     # otherwise, do not delete
     if patient_id in patients:
+        # iterate through all the exam ids that are in the patient's exam set()
+        # then delete them from the exam record's dict
+        for exam_id in patients[patient_id][1]:
+            exams.pop(exam_id)
+
+        # delete the patient from the patients record dict
         patients.pop(patient_id)
 
 
-def delete_exam(patients, exam_id):
+def delete_exam(patients, exams, exam_id):
     """
     delete the given exam
+    :param exams: dict of exam ids
     :param patients: dict of patients
     :param exam_id: exam id
     """
 
-    # iterate through the patients dict searching for the patient who owns that exam record
-    # if an owner of that exam record is found, delete the exam record
-    # otherwise, do not delete
-    for patient_id in patients:
-        if exam_id in patients[patient_id][1]:
+    # check if an exam with that id exists
+    # if the exam exists, delete the exam from patient's set who
+    #   owns that exam and the general record of exam ids\
+    # otherwise, continue processing
+    if exam_id in exams:
+        patient_id = exams.get(exam_id)
+        if patient_id in patients:
             patients[patient_id][1].remove(exam_id)
-            break
+        exams.pop(exam_id)
 
 
-def process_instruction(patients, instruction):
+def process_instruction(patients, exams, instruction):
     """
     process the passed in valid instruction
+    :param exams: dict of exam ids
     :param patients: dict of patients
     :param instruction: valid instruction
     """
@@ -107,21 +120,22 @@ def process_instruction(patients, instruction):
     elif command == "ADD" and record == "EXAM":
         patient_id = instr_list[2]
         exam_id = instr_list[3]
-        add_exam(patients, patient_id, exam_id)
+        add_exam(patients, exams, patient_id, exam_id)
     # DEL PATIENT patient_iD
     elif command == "DEL" and record == "PATIENT":
         patient_id = instr_list[2]
-        delete_patient(patients, patient_id)
+        delete_patient(patients, exams, patient_id)
     # DEL EXAM exam_id
     elif command == "DEL" and record == "EXAM":
         exam_id = instr_list[2]
-        delete_exam(patients, exam_id)
+        delete_exam(patients, exams, exam_id)
 
 
 def process_file(file_name):
     """
     process the given file of Patient and Exam records
     :param file_name: name of input file
+    :return: a summary of the successfully processed file as a string
     """
 
     # set of all valid command actions and records
@@ -132,6 +146,13 @@ def process_file(file_name):
     #   key = id
     #   val = [name, set() of exam ids]
     patients = {}
+
+    # dictionary containing all the current exams
+    # used for maintaining a record of all the current ids
+    # used to prevent two separate patients to have the same exam id
+    #   key = exam_id
+    #   value = patient_id
+    exams = {}
 
     # parse file line by line
     try:
@@ -146,7 +167,7 @@ def process_file(file_name):
             if line_seg[0] in valid_command and line_seg[1] in valid_record:
                 # valid instruction
                 # therefore, process the instruction
-                process_instruction(patients, line)
+                process_instruction(patients, exams, line)
     except FileNotFoundError:
         print("File :", file_name + " does not exist")
         sys.exit(-1)
